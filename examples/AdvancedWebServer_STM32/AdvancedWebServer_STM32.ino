35cbcab8f2b4171f77e65d5346500ab81413dcdb
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-07-26 17:54:48 -0400

    v1.0.12
    ### New in v1.0.12
    
    1. Add sample Packages_Patches for STM32 (Nucleo-144 NUCLEO_F767ZI, Nucleo-64 NUCLEO_L053R8)
    2. Add example for STM32 boards.
    3. Add instructions to use EspSerial/Serial1 on some STM32 boards without Serial1 definition (Nucleo-144 NUCLEO_F767ZI, Nucleo-64 NUCLEO_L053R8, etc.)

--- examples/AdvancedWebServer_STM32/AdvancedWebServer_STM32.ino
+++ examples/AdvancedWebServer_STM32/AdvancedWebServer_STM32.ino
@@ -35,10 +35,10 @@
     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-    Version: 1.0.11
+    Version: 1.0.12
 
-    Version Modified By   Date      Comments
-    ------- -----------  ---------- -----------
+   Version Modified By   Date      Comments
+   ------- -----------  ---------- -----------
     1.0.0   K Hoang      12/02/2020 Initial coding for Arduino Mega, Teensy, etc
     1.0.1   K Hoang      17/02/2020 Add support to server's lambda function calls
     1.0.2   K Hoang      22/02/2020 Add support to SAMD (DUE, ZERO, MKR, NANO_33_IOT, M0, M0 Pro, AdaFruit, etc) boards
@@ -51,7 +51,8 @@
     1.0.8   K Hoang      01/07/2020 Fix bug. Add features to ESP32-AT.   
     1.0.9   K Hoang      03/07/2020 Fix bug. Add functions. Restructure codes.
     1.0.10  K Hoang      22/07/2020 Fix bug not closing client and releasing socket.
-    1.0.11  K Hoang      25/07/2020 Add support to all STM32F/L/H/G/WB/MP1 and Seeeduino SAMD21/SAMD51 boards         
+    1.0.11  K Hoang      25/07/2020 Add support to all STM32F/L/H/G/WB/MP1 and Seeeduino SAMD21/SAMD51 boards  
+    1.0.12  K Hoang      26/07/2020 Add example and sample Packages_Patches for STM32F/L/H/G/WB/MP boards         
  *****************************************************************************************************************************/
 
 // Credits of [Miguel Alexandre Wisintainer](https://github.com/tcpipchip) for this simple yet effective method
